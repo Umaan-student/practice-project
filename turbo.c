c3d410d68855a7b990bb17feb88c9603b8e7c8c4
Umaan-student <umantomail@gmail.com>
2025-10-18 16:01:36 +0530

    upload new file in turbo IDE format

---
+++ turbo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+printf("Hello World!");
+printf("Hello World!");
+printf("Hello World!");
+printf("Hello World!");
+printf("From developer");
+getch();
+}
