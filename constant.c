16ab9921d604b560a50c016a097bb3c55314db17
Umaan-student <umantomail@gmail.com>
2025-10-20 12:21:01 +0530

    upload new file on git hub constant file is main

---
+++ constant.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main(){
+/*{const float pi = 7.14;
+
+printf("%f", pi);}
+const float pi = 1.43;
+printf("\n\n%f", pi);*/
+
+int num1 = 20;
+const float num = 100.2;
+
+printf("%.f", num);
+
+return 0;
+}
